cec86e8cd7a00caae9345a462bff702b67b30632
xieqinghe <qinghe101@qq.com>
2017-11-21 16:08:10 +0800

    start class rename

--- src/main/java/com/hehe/security/Application.java
+++ src/main/java/com/hehe/security/Application.java
@@ -7,9 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @author hehe
  */
 @SpringBootApplication
-public class SpringSecurityOauth2Application {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringSecurityOauth2Application.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 }
